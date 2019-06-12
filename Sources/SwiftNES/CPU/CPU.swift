76b85889d43132c9c6ba514caecea1fd0837fc3c
Tomochika Hara <git@thara.jp>
2019-06-12 23:21:42 +0900

    :name_badge: Rename CPU.swift

--- Sources/SwiftNES/CPU/CPU.swift
+++ Sources/SwiftNES/CPU/CPU.swift
@@ -1,13 +1,13 @@
 struct OpCode {}
 struct Instruction {}
 
-protocol Cpu {
+protocol CPU {
     func fetch() -> OpCode
     func decode(_ opcode: OpCode) -> Instruction
     func execute(_ instraction: Instruction)
 }
 
-extension Cpu {
+extension CPU {
     func run() {
         let opcode = fetch()
         let instruction = decode(opcode)
