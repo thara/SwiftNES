ffec56b0d5e558c71ec9f3d51a60f7dd5d7fd837
Tomochika Hara <git@thara.jp>
2019-07-29 18:05:01 +0900

    :wrench: Add loggers

--- Sources/SwiftNES/Logging.swift
+++ Sources/SwiftNES/Logging.swift
@@ -3,5 +3,8 @@ import Logging
 public var cpuLogger = Logger(label: "SwiftNES.CPU")
 
 public var ppuLogger = Logger(label: "SwiftNES.PPU")
+public var ppuBusLogger = Logger(label: "SwiftNES.PPUBus")
+public var ppuBackgroundLogger = Logger(label: "SwiftNES.Background")
 
 public var interruptLogger = Logger(label: "SwiftNES.Interrupt")
+
