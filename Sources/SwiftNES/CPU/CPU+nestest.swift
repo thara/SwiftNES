6c7885571c6c067f51a2598df23affbde9967001
Tomochika Hara <git@thara.jp>
2019-08-20 02:21:29 +0900

    :rotating_light: Fix nestest log

--- Sources/SwiftNES/CPU/CPU+nestest.swift
+++ Sources/SwiftNES/CPU/CPU+nestest.swift
@@ -29,7 +29,7 @@ extension CPU {
             case .immediate:
                 operand = UInt16(operand1)
             case .relative:
-                operand += (pc + 1 + operandPC)
+                operand = UInt16(Int(pc) + 2 + Int(operand.i8))
             default:
                 break
             }
