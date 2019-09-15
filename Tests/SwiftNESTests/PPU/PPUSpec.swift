4b2435107205444c6464d254cc61f6574f55bd49
Tomochika Hara <git@thara.jp>
2019-09-15 22:55:46 +0900

    :rotating_light: Fix tests

--- Tests/SwiftNESTests/PPU/PPUSpec.swift
+++ Tests/SwiftNESTests/PPU/PPUSpec.swift
@@ -11,7 +11,7 @@ class PPUSpec: QuickSpec {
                 ppu = PPU()
             }
 
-            describe("updateBackground") {
+            describe("fetchBackgroundPixel") {
                 beforeEach {
                     ppu.registers.v = vramAddress(fineYScroll: 5, nameTableNo: 2, coarseYScroll: 0b11001, coarseXScroll: 0b11101)
 
@@ -26,35 +26,35 @@ class PPUSpec: QuickSpec {
                 it("update background state") {
                     _ = ppu.scan.nextDot()
 
-                    ppu.updateBackground()
+                    ppu.fetchBackgroundPixel()
                     expect(ppu.background.tempTableAddr).to(equal(0x2B3D))
 
                     _ = ppu.scan.nextDot()
-                    ppu.updateBackground()
+                    ppu.fetchBackgroundPixel()
                     expect(ppu.background.nameTableEntry).to(equal(0x03))
 
                     _ = ppu.scan.nextDot()
-                    ppu.updateBackground()
+                    ppu.fetchBackgroundPixel()
                     expect(ppu.background.tempTableAddr).to(equal(0x2BF7))
 
                     _ = ppu.scan.nextDot()
-                    ppu.updateBackground()
+                    ppu.fetchBackgroundPixel()
                     expect(ppu.background.attrTableEntry).to(equal(0x41))
 
                     _ = ppu.scan.nextDot()
-                    ppu.updateBackground()
+                    ppu.fetchBackgroundPixel()
                     expect(ppu.background.tempTableAddr).to(equal(0x0035))
 
                     _ = ppu.scan.nextDot()
-                    ppu.updateBackground()
+                    ppu.fetchBackgroundPixel()
                     expect(ppu.background.tempTileFirst).to(equal(0x11))
 
                     _ = ppu.scan.nextDot()
-                    ppu.updateBackground()
+                    ppu.fetchBackgroundPixel()
                     expect(ppu.background.tempTableAddr).to(equal(0x003D))
 
                     _ = ppu.scan.nextDot()
-                    ppu.updateBackground()
+                    ppu.fetchBackgroundPixel()
                     expect(ppu.background.tempTileSecond).to(equal(0x81))
                 }
             }
