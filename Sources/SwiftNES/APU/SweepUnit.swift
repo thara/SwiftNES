b8ac7be9cf899e13f73dc4f9054f32326401f4d8
Tomochika Hara <git@thara.jp>
2019-12-11 22:56:34 +0900

    :construction: Add SweepUnit

---
+++ Sources/SwiftNES/APU/SweepUnit.swift
@@ -0,0 +1,68 @@
+// http://wiki.nesdev.com/w/index.php/APU_Sweep
+class SweepUnit {
+
+    var reloadFlag = false
+
+    var enabled = false
+    var negate = false
+    var shiftCount: UInt16 = 0
+
+    var targetPeriod: UInt16 = 0
+
+    lazy var divider: Divider = {
+        Divider { self.timer?.update(newPeriod: self.targetPeriod) }
+    }()
+    var timer: Timer?
+
+    init() {
+    }
+
+    func update(by data: UInt8) {
+        enabled = data[7] == 1
+        divider.updatePeriod(using: (data & 0b01110000) >> 4)
+        negate = data[3] == 1
+        shiftCount = UInt16(data & 0b111)
+
+        reloadFlag = true
+    }
+
+    func connect(to timer: Timer) -> Timer {
+        self.timer = timer
+        return timer
+    }
+
+    func calculateTargetPeriod(_ period: UInt16) -> UInt16 {
+        // TODO
+        // Pulse 1 adds the ones' complement (−c − 1). Making 20 negative produces a change amount of −21.
+        // Pulse 2 adds the two's complement (−c). Making 20 negative produces a change amount of −20.
+        let changeAmount = Int(period >> shiftCount) * (negate ? -1 : 1)
+        return UInt16(Int(period) &+ changeAmount)
+    }
+
+    var mutated: Bool {
+        return 0x7FF < targetPeriod
+    }
+
+    func clock(rawTimerPeriod: UInt16) {
+        if divider.zero && enabled && !mutated {
+            // The pulse's period is adjusted.
+            targetPeriod = calculateTargetPeriod(rawTimerPeriod)
+        }
+        //FIXME Modify divider internal state
+        if divider.zero || reloadFlag {
+            reloadFlag = false
+            divider.reload()
+            divider.nextClock()
+        } else {
+            divider.counter &-= 1
+        }
+    }
+
+    func gate(input: UInt16) -> UInt16 {
+        if enabled {
+            return input
+        } else {
+            return 0
+        }
+    }
+}
---
+++ Tests/SwiftNESTests/APU/SweepUnitSpec.swift
@@ -0,0 +1,27 @@
+import Quick
+import Nimble
+
+@testable import SwiftNES
+
+class SweepUnitSpec: QuickSpec {
+    override func spec() {
+        var sweepUnit: SweepUnit!
+        beforeEach {
+            sweepUnit = SweepUnit()
+        }
+
+        describe("update") {
+            it("updates properties from passed data") {
+                sweepUnit.update(by: 0b10101010)
+
+                expect(sweepUnit.enabled) == true
+                expect(sweepUnit.divider.period) == 0b011
+                expect(sweepUnit.negate) == true
+                expect(sweepUnit.shiftCount) == 0b010
+                expect(sweepUnit.reloadFlag) == true
+            }
+        }
+
+        //TODO WIP
+    }
+}
